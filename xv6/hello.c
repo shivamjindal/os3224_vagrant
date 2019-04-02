da3f6fbe96b6de091351533cad4610debb907013
Shivam Jindal <jshivam21@gmail.com>
2019-04-02 18:29:03 -0400

    homework 1 and homework 2

--- xv6/hello.c
+++ xv6/hello.c
@@ -1,7 +1,8 @@
 #include "types.h"
 #include "user.h"
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 	printf(1, "Hello world\n");
 	exit();
 }
--- xv6/uniq.c
+++ xv6/uniq.c
@@ -40,7 +40,7 @@ void addTo(struct line *givenLine, char *newChar)
   givenLine->lineString[++(givenLine->lineSize)] = '\0';
 }
 
-int areStringsTheSame(struct line *line1, struct line *line2)
+int areStringsTheSame(struct line *line1, struct line *line2, int iFlag)
 {
   int result, i;
   result = 1;
@@ -53,18 +53,35 @@ int areStringsTheSame(struct line *line1, struct line *line2)
   {
     for (i = 0; i < line1->lineSize; i++)
     {
-      if (line1->lineString[i] != line2->lineString[i])
+      if (line1->lineString[i] != line2->lineString[i] && !iFlag)
       {
-        result = 0;
+        return 0;
+      }
+      else if (iFlag)
+      {
+        int diff;
+        diff = line1->lineString[i] - line2->lineString[i];
+        if (diff == 0)
+        {
+          continue;
+        }
+        else if (diff < 0)
+        {
+          diff *= -1;
+        }
+        if (diff != 32 && diff != 0)
+        {
+          return 0;
+        }
       }
     }
   }
   return result;
 }
 
-void uniq(int fd, struct line *line1, struct line *line2)
+void uniq(int fd, struct line *line1, struct line *line2, int iFlag, int cFlag, int dFlag)
 {
-  int n, addingLine1, addingLine2;
+  int n, addingLine1, addingLine2, counter;
   addingLine1 = 1;
   addingLine2 = 0;
 
@@ -77,6 +94,7 @@ void uniq(int fd, struct line *line1, struct line *line2)
       {
         addingLine1 = 0;
         addingLine2 = 1;
+        counter = 1;
       }
     }
     else if (addingLine2)
@@ -86,24 +104,30 @@ void uniq(int fd, struct line *line1, struct line *line2)
       {
         addingLine1 = 0;
         addingLine2 = 0;
-      }
-    }
-    if (!(addingLine2) && !(addingLine1))
-    {
-      if (areStringsTheSame(line1, line2))
-      {
-        initializeLine(line2);
-        addingLine2 = 1;
-      }
-      else
-      {
-        printf(1, "%s", line1->lineString);
-        free(line1->lineString);
-        line1->lineString = line2->lineString;
-        line1->lineSize = line2->lineSize;
-        line1->lineCap = line2->lineCap;
-        initializeLine(line2);
-        addingLine2 = 1;
+        if (areStringsTheSame(line1, line2, iFlag))
+        {
+          initializeLine(line2);
+          addingLine2 = 1;
+          counter++;
+        }
+        else
+        {
+          if (cFlag)
+          {
+            printf(1, "%d %s", counter, line1->lineString);
+          }
+          else if (!dFlag || counter > 1)
+          {
+            printf(1, "%s", line1->lineString);
+          }
+          counter = 1;
+          free(line1->lineString);
+          line1->lineString = line2->lineString;
+          line1->lineSize = line2->lineSize;
+          line1->lineCap = line2->lineCap;
+          initializeLine(line2);
+          addingLine2 = 1;
+        }
       }
     }
   }
@@ -113,33 +137,61 @@ void uniq(int fd, struct line *line1, struct line *line2)
     printf(1, "uniq: read error\n");
     exit();
   }
-  printf(1, "%s", line1->lineString);
-  printf(1, "%s", line2->lineString);
+
+  if (cFlag)
+  {
+    printf(1, "%d %s", counter, line1->lineString);
+  }
+  else if (!dFlag || counter > 1)
+  {
+    printf(1, "%s", line1->lineString);
+  }
+
   free(line1->lineString);
   free(line2->lineString);
 }
 
 int main(int argc, char *argv[])
 {
-  int fd;
+  int fd, i, iFlagBool, cFlagBool, dFlagBool;
   struct line line1, line2;
+
+  iFlagBool = 0, cFlagBool = 0, dFlagBool = 0;
+  char I_FLAG[3] = {'-', 'i', '\0'};
+  char C_FLAG[3] = {'-', 'c', '\0'};
+  char D_FLAG[3] = {'-', 'd', '\0'};
 
   initializeLine(&line1);
   initializeLine(&line2);
 
   if (argc <= 1)
   {
-    uniq(0, &line1, &line2);
+    uniq(0, &line1, &line2, iFlagBool, cFlagBool, dFlagBool);
     exit();
   }
 
-  if ((fd = open(argv[1], 0)) < 0)
+  for (i = 1; i < argc; i++)
   {
-    printf(1, "uniq: cannot open %s\n", argv[1]);
-    exit();
+    if (strcmp(argv[i], I_FLAG) == 0)
+    {
+      iFlagBool = 1;
+    }
+    else if (strcmp(argv[i], C_FLAG) == 0)
+    {
+      cFlagBool = 1;
+    }
+    else if (strcmp(argv[i], D_FLAG) == 0)
+    {
+      dFlagBool = 1;
+    }
+    else if ((fd = open(argv[i], 0)) < 0)
+    {
+      printf(1, "uniq: cannot open %s\n", argv[i]);
+      exit();
+    }
   }
 
-  uniq(fd, &line1, &line2);
+  uniq(fd, &line1, &line2, iFlagBool, cFlagBool, dFlagBool);
   close(fd);
 
   exit();
